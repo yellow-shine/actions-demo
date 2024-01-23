051483d80d7b529fe91544cd04f52c2769695412
sammy.huang <sammy.huang@zilliz.com>
2024-01-23 17:07:57 +0800

    Update hello.sh

--- .github/workflows/hello.sh
+++ .github/workflows/hello.sh
@@ -7,4 +7,4 @@ MILVUS_CORE_DIR="${ROOT_DIR}/internal/core"
 MILVUS_CORE_UNITTEST_DIR="${MILVUS_CORE_DIR}/output/unittest"
 echo ${${MILVUS_CORE_UNITTEST_DIR}/}/${test} "run failed"
 
-exit 1
+exit 2
